c53fa980bd0e396f8404d22970aad37b32aabbe2
namita <namitasanthosh13@gmail.com>
2020-11-26 23:32:51 +0530

    init setup

---
+++ truyum-spring-mvc/src/main/java/com/cognizant/truyum/dao/CartEmptyException.java
@@ -0,0 +1,14 @@
+package com.cognizant.truyum.dao;
+
+@SuppressWarnings("serial")
+public class CartEmptyException extends Exception {
+
+	public CartEmptyException() {
+		super();
+	}
+
+	public CartEmptyException(String message) {
+		super(message);
+	}
+
+}
